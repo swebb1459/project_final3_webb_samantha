574ed71c6d33667ed756aae8ee53de79b8e34856
Samantha Webb <samanthawebb@outlook.com>
2016-04-20 19:21:47 -0400

    write jQuery code to change color of nav background

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
 
     //Code below is for MixItUp jQuery plugin
-    $(function(){
-    $('#menu').mixItUp();
-});
+    $(function() {
+        $('#menu').mixItUp();
+    });
+
+    //jQuery code that changes the color of the navigation when
+    //when you mouseenter and mouseleave the main image on the homepage.
+    $('#main_image').mouseenter(function() {
+        console.log('working');
+        $('nav').css('background', '#192860');
+    });
+
+    $('#main_image').mouseleave(function() {
+        console.log('working');
+        $('nav').css('background', '#9cca3d');
+    });
 });
